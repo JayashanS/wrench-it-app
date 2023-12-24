31b98d0d8049a7c82a09fdb941ff6196c783ab6c
Himantha-alt <jayashanhimantha@gmail.com>
2023-12-24 22:43:39 +0530

    Updated View

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard.js";
 import User from "./pages/User.js";
+import Appbar from "./components/Appbar.js";
 function App() {
-  return <User></User>;
+  return (
+    <>
+      <Appbar></Appbar>
+      <User></User>
+    </>
+  );
 }
 
 export default App;
--- admin/src/pages/User.js
+++ admin/src/pages/User.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {
+  ThemeProvider,
+  createTheme,
   Table,
   TableHead,
   TableBody,
@@ -17,7 +19,7 @@ const User = () => {
       try {
         const response = await fetch("http://localhost:4000/api/garage");
         const jsonData = await response.json();
-        setData(jsonData);
+        setData(sortData(jsonData));
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
@@ -28,52 +30,92 @@ const User = () => {
 
   const handleDelete = async (garageId) => {
     try {
-      const response = await fetch(
-        `http://localhost:4000/api/garage//${garageId}`,
-        {
-          method: "DELETE",
-        }
-      );
+      await fetch(`http://localhost:4000/api/garage/${garageId}`, {
+        method: "DELETE",
+      });
 
-      const responseData = await response.json();
-      console.log("Response after delete:", responseData);
-
-      // Remove the deleted item from the state
       setData((prevData) => prevData.filter((item) => item._id !== garageId));
     } catch (error) {
       console.error("Error deleting data: ", error);
     }
   };
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#4e03fc",
+        contrastText: "#fff",
+      },
+      secondary: {
+        main: "#57a8b5",
+        contrastText: "#fff",
+      },
+      background: {
+        paper: "#060321",
+      },
+      text: {
+        primary: "#05e0e8",
+        secondary: "#a9b4c0",
+      },
+    },
+  });
+
+  const sortData = (data = [], sortBy = "garageId", ascending = true) => {
+    return data.sort((a, b) => {
+      const aValue = a[sortBy] ?? "";
+      const bValue = b[sortBy] ?? "";
+      return ascending
+        ? aValue.localeCompare(bValue)
+        : bValue.localeCompare(aValue);
+    });
+  };
+
   return (
-    <Paper>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Delete</TableCell> {/* New column for delete button */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((item) => (
-            <TableRow key={item._id}>
-              <TableCell>{item._id}</TableCell>
-              <TableCell>{item.ownerName}</TableCell>
-              <TableCell>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => handleDelete(item._id)}
-                >
-                  Delete
-                </Button>
-              </TableCell>
+    <ThemeProvider theme={darkTheme}>
+      <Paper>
+        <Table stickyHeader sx={{ minWidth: 650 }}>
+          <TableHead>
+            <TableRow sx={{ justifyContent: "center" }}>
+              <TableCell></TableCell>
+              <TableCell>ID</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Address</TableCell>
+              <TableCell>Owner</TableCell>
+              <TableCell>No. of Workers</TableCell>
+              <TableCell />
+              <TableCell />
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+          </TableHead>
+          <TableBody>
+            {data.map((item) => (
+              <TableRow key={item._id} sx={{ justifyContent: "center" }}>
+                <TableCell></TableCell>
+                <TableCell>{item.garageId}</TableCell>
+                <TableCell>{item.repairCenterName}</TableCell>
+                <TableCell>{item.address}</TableCell>
+                <TableCell>{item.ownerName}</TableCell>
+                <TableCell>{item.numOfWorkers}</TableCell>
+                <TableCell>
+                  <Button variant="outlined" color="primary">
+                    View
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => handleDelete(item._id)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
