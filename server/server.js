0ae00026bfb44be31d83165f0ae574259834639b
kavishka-sandaruwan <kavishkasandaruwan03@gmail.com>
2024-01-06 11:41:20 +0530

    send second time

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 //const ownerRoutes = require('./routes/owner')
 //const vehicleRoutes = require('./routes/vehicle')
-//const requestRoutes = require('./routes/request')
+const requestRoutes = require('./routes/request')
 //const repairRoutes = require('./routes/repair')y
 const garageRoutes = require("./routes/garage");
 //const partRoutes = require('./routes/part')
@@ -23,6 +23,21 @@ app.use(
 
 //routes
 app.use("/api/garage", garageRoutes);
+app.use("/api/request", requestRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
