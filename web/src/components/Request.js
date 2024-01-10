e8b3ba79bcb2165fcf4a006e0117fc3b60805c9e
Himantha-alt <jayashanhimantha@gmail.com>
2024-01-10 15:29:14 +0530

    Update Request.js

--- web/src/components/Request.js
+++ web/src/components/Request.js
@@ -1,17 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../styles/Request.css";
 
-function Request(){
-
-    return(
-        <div className="container-request">
-            <div className="process-request">
-                <div className="incoming-request"></div>
-                <div className="accepted-request"></div>
-            </div>
-            <div className="request-details"></div>
-        </div>
-    );
+function Request() {
+  return (
+    <div className="container-request">
+      <div className="process-request">
+        <div className="incoming-request"></div>
+        <div className="accepted-request"></div>
+      </div>
+      <div className="request-details"></div>
+    </div>
+  );
 }
 
 export default Request;
