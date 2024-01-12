fa2c8e14a20904ce9bf6339b69427dc6058d55f3
Himantha-alt <jayashanhimantha@gmail.com>
2024-01-12 13:08:15 +0530

    Corrected

--- server/controllers/vehicleController.js
+++ server/controllers/vehicleController.js
@@ -1,44 +1,52 @@
 const Vehicle = require("../models/vehicleModel");
 
-// Creating Owner
+// Creating Vehicle
 const createVehicle = async (req, res) => {
-  const { licensePlateNumber,manufacturer,model,category,year,fault,nic } =
-    req.body;
+  const {
+    licencePlateNumber,
+    manufacturer,
+    model,
+    category,
+    year,
+    fault,
+    nic,
+  } = req.body;
 
   try {
     const vehicle = await Vehicle.create({
-        licensePlateNumber,
-        manufacturer,
-        model,
-        category,
-        year,
-        fault,
-        nic
+      licencePlateNumber,
+      manufacturer,
+      model,
+      category,
+      year,
+      fault,
+      nic,
     });
     res.status(201).json(vehicle);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Requested information is incorrect!" });
   }
 };
 
-const deleteVehiclebyLIC= async (req, res) => {
-    try {
-      const LIC = req.params.id;
-      const deletedVehicle = await Owner.findOneAndDelete({ licensePlateNumber:LIC });
-  
-      if (!deletedVehicle) {
-        return res
-          .status(404)
-          .json({ message: "Vehicle not Found", licensePlateNumber: LIC });
-      }
-  
-      return res.status(200).json({ message: "Vehicle successfully deleted" });
-    } catch (error) {
-      console.error("Error deleting Vehicle", Vehicle);
-      return res.status(500).json({ error: "Could not delete Vehicle" });
+const deleteVehiclebyLIC = async (req, res) => {
+  try {
+    const LIC = req.params.id;
+    const deletedVehicle = await Vehicle.findOneAndDelete({
+      licencePlateNumber: LIC,
+    });
+
+    if (!deletedVehicle) {
+      return res
+        .status(404)
+        .json({ message: "Vehicle not Found", licensePlateNumber: LIC });
     }
-  };
-  
-  module.exports = { createVehicle, deleteVehiclebyLIC};
 
+    return res.status(200).json({ message: "Vehicle successfully deleted" });
+  } catch (error) {
+    console.error("Error deleting Vehicle", error);
+    return res.status(500).json({ error: "Could not delete Vehicle" });
+  }
+};
 
+module.exports = { createVehicle, deleteVehiclebyLIC };
