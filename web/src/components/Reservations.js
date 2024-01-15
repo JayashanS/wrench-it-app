34858365291e82582df0d58aaa4c43f53ede49ed
kavishka-sandaruwan <kavishkasandaruwan03@gmail.com>
2024-01-15 20:34:46 +0530

    add

--- web/src/components/Reservations.js
+++ web/src/components/Reservations.js
@@ -11,17 +11,29 @@ const Reservations = () => {
       <div class="column-3">
         <div class="innerColumn-1">
           <div class="circle-kawishka">
+            
 
           </div>
+          <p><center><span class="container-title">john Smith</span></center></p>
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          <input type="checkbox" id="service1" name="service1" value="service 1"/>
+          <label for="service1"> Service 1</label>
+          <input type="checkbox" id="service1" name="service1" value="service 1"/>
+          <label for="service1"> Service 1</label>
+          <input type="checkbox" id="service1" name="service1" value="service 1"/>
+          <label for="service1"> Service 1</label>
+
+
+
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+
+          <p><span class="container-title">Date</span></p>
+  
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          <p><span class="container-title">Time</span></p>
         </div>
 
           
@@ -29,31 +41,31 @@ const Reservations = () => {
 
       <div class="column-3">
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+         
         </div>
           
       </div>
       <div class="column-3">
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
         <div class="innerColumn-1">
-          ddddddddddddddd
+          
         </div>
          
       </div>
@@ -64,7 +76,7 @@ const Reservations = () => {
       <p ><span class="container-title">Accepted</span></p>
       <div class="column-4">
 
-          ddddd
+          dddd
       </div>
 
       <div class="column-4">
