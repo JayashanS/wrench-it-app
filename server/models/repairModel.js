03c7575f8863431db297f9fc7fb2d32ce2fea8c4
Himantha-alt <jayashanhimantha@gmail.com>
2024-01-17 14:16:21 +0530

    Repair Fixed

--- server/models/repairModel.js
+++ server/models/repairModel.js
@@ -1,46 +1,43 @@
-const mongoose=require('mongoose')
-const Schema=mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const repairSchema=new Schema({
-    repairId:{
-        type:String,
-        required:true,
-        unique:true
-     },
-    licensePlateNo:{
-        type: String,
-        required:true
-        
-    },
-    model:{
-        type:String,
-        required:true
-    },
+const repairSchema = new Schema({
+  repairId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  licensePlateNo: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
 
-    fault:{
-        type:String,
-        required:true
-    },
+  fault: {
+    type: String,
+    required: true,
+  },
 
-    NIC:{
-        type:String,
-        required:true
-    },
+  NIC: {
+    type: String,
+    required: true,
+  },
 
-    phoneNo:{
-        type:String,
-        required:true
-    },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
 
-    date:{
-        type:Date,
-        required:true
-    },
+  date: {
+    type: Date,
+    required: true,
+  },
 
-    status:{
-        type:String,
-        required:true
-    }
-
-})
-module.exports=mongoose.model('Repair',repairSchema)
+  status: {
+    type: String,
+  },
+});
+module.exports = mongoose.model("Repair", repairSchema);
--- web/src/components/Repair.js
+++ web/src/components/Repair.js
@@ -1,174 +1,194 @@
 import React, { useState } from "react";
 import { Accordion, Card } from "react-bootstrap";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import "../styles/Repair.css";
-import {
-  createTheme,
-  ThemeProvider,
-  getContrastRatio,
-} from "@mui/material/styles";
 
 function Repair() {
-	const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
   const [isPartsFormOpen, setIsPartsFormOpen] = useState(false);
-  
+  const [repairId, setRepairId] = useState("");
+  const [licensePlateNo, setlicensePlateNo] = useState("");
+  const [model, setmodel] = useState("");
+  const [fault, setfault] = useState("");
+  const [NIC, setNIC] = useState("");
+  const [date, setdate] = useState("");
+  const [phoneNo, setphoneNo] = useState("");
+  const [status, setstatus] = useState("");
+  const [error, setError] = useState(null);
 
-	const toggleAccordion = () => {
-	  setIsAccordionOpen(!isAccordionOpen);
-	};
+  const toggleAccordion = () => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
   const togglePartsForm = () => {
     setIsPartsFormOpen(!isPartsFormOpen);
   };
 
-  const theme = createTheme({
-    typography: {
-      fontFamily: "Roboto, sans-serif",
-    },
-    palette: {
-      primary: {
-        main: "#09BEB1",
-        contrastText: getContrastRatio("#09BEB1", "#fff") > 4.5 ? "#fff" : "#111",
+  const handleSubmit = async (e) => {
+    console.log("button clicked");
+    e.preventDefault();
+    const repair = {
+      repairId,
+      licensePlateNo,
+      model,
+      fault,
+      NIC,
+      date,
+      phoneNo,
+      status,
+    };
+    console.log("Form Data:", repair);
+
+    const response = await fetch("http://localhost:4000/api/repair/", {
+      method: "POST",
+      body: JSON.stringify(repair),
+      headers: {
+        "Content-Type": "application/json",
       },
-    },
-  });
+    });
+    console.log("button clicked");
 
-  
-    const[repairId,setRepairId]=useState('')
-    const[licensePlateNo,setlicensePlateNo]=useState('')
-    const[model,setmodel]=useState('')
-    const[fault,setfault]=useState('')
-    const[NIC,setNIC]=useState('')
-    const[date,setdate]=useState('')
-    const[phoneNo,setphoneNo]=useState('')
-    const[status,setstatus]=useState('')
-    const[error,setError]=useState(null)
+    const json = await response.json();
 
-   const handleSubmit=async(e)=>{
-    console.log('button clicked')
-      e.preventDefault()
-      const  repair={repairId,licensePlateNo,model,fault,NIC,date,phoneNo,status}
-     
-      const response=await fetch('/api/repair',{
-        method:'POST',
-        body:JSON.stringify(repair),
-        headers:{
-          'Content-Type':'application/json'
-        }
-      })
-      console.log('button clicked')
-
-      const json=await response.json()
-
-      if(!response.ok){
-          setError(json.error)
-      }
-      if(response.ok){
-        setRepairId('')
-        setlicensePlateNo('')
-        setmodel('')
-        setfault('')
-        setNIC('')
-        setdate('')
-        setphoneNo('')
-        setstatus('')
-        setError(null)
-        console.log('new workout added',json)
-      }
-   }
- 
-
-
- 
-// form validation
-/*const validateForm = () => {
-    const form = document.getElementById("repairForm");
-    const elements = form.elements;
-
-    for (let i = 0; i < elements.length; i++) {
-      if (elements[i].type !== "button" && elements[i].type !== "submit") {
-        if (!elements[i].value.trim()) {
-          alert("Please fill in all required fields.");
-          return;
-        }
-      }
+    if (!response.ok) {
+      setError(json.error);
     }
-    alert("Form submitted successfully!");
+    if (response.ok) {
+      setRepairId("");
+      setlicensePlateNo("");
+      setmodel("");
+      setfault("");
+      setNIC("");
+      setdate("");
+      setphoneNo("");
+      setstatus("");
+      setError(null);
+      console.log("new workout added", json);
+    }
   };
 
-*/
-
-
-//const Repair = () => {
   return (
     <div class="RepairPage">
       <div class="RepairContainer1">
         <h2>Details</h2>
         <hr />
         <br />
-        <ThemeProvider theme={theme}>
+
         <Stack spacing={2} direction="row">
-    
-      <Button variant="contained" onClick={toggleAccordion} style={{ color: 'white',textTransform:'none' }}>Add new Repair Record  </Button>
-
-    </Stack>
-    </ThemeProvider>
+          <Button
+            variant="contained"
+            onClick={toggleAccordion}
+            style={{ color: "white", textTransform: "none" }}
+          >
+            Add new Repair Record{" "}
+          </Button>
+        </Stack>
 
         <br />
         <br />
-		{isAccordionOpen && (
+        {isAccordionOpen && (
+          //Add new Repair form
 
-//Add new Repair form
+          <div className="Add-new-Form">
+            <Card className="Add-New-Form-card">
+              <Card.Body>
+                <h2>Repair Information Form</h2>
+                <form id="repairForm" onSubmit={handleSubmit}>
+                  <label for="repairId">Repair ID:</label>
+                  <input
+                    type="text"
+                    id="repairId"
+                    name="repairId"
+                    onChange={(e) => setRepairId(e.target.value)}
+                    value={repairId}
+                    required
+                  />
 
-        <div className="Add-new-Form">
-          <Card className="Add-New-Form-card">
-            <Card.Body>
-			
-		<h2>Repair Information Form</h2>
-			<form id="repairForm" onSubmit={handleSubmit}>
-				<label for="repairId">Repair ID:</label>
-    			<input type="text" id="repairId" name="repairId" onChange={(e)=>setRepairId(e.target.value)} value={repairId} required/>
+                  <label for="licensePlateNo">License Plate No:</label>
+                  <input
+                    type="text"
+                    id="licensePlateNo"
+                    name="licensePlateNo"
+                    onChange={(e) => setlicensePlateNo(e.target.value)}
+                    value={licensePlateNo}
+                    required
+                  ></input>
 
-				<label for="licensePlateNo">License Plate No:</label>
-   	 			<input type="text" id="licensePlateNo" name="licensePlateNo" onChange={(e)=>setlicensePlateNo(e.target.value)} value={licensePlateNo} required></input>
-				
-				<label for="model">Model:</label>
-    			<input type="text" id="model" name="model" onChange={(e)=>setmodel(e.target.value)} value={model} required/>
+                  <label for="model">Model:</label>
+                  <input
+                    type="text"
+                    id="model"
+                    name="model"
+                    onChange={(e) => setmodel(e.target.value)}
+                    value={model}
+                    required
+                  />
 
-				<label for="fault">Fault:</label>
-    			<textarea id="fault" name="fault" rows="4" onChange={(e)=>setfault(e.target.value)} value={fault} required></textarea>
-				
-				<label for="NIC">NIC:</label>
-    			<input type="text" id="NIC" name="NIC" onChange={(e)=>setNIC(e.target.value)} value={NIC} required />
+                  <label for="fault">Fault:</label>
+                  <textarea
+                    id="fault"
+                    name="fault"
+                    rows="4"
+                    onChange={(e) => setfault(e.target.value)}
+                    value={fault}
+                    required
+                  ></textarea>
 
-   				 <label for="phoneNo">Phone No:</label>
-    			<input type="tel" id="phoneNo" name="phoneNo" onChange={(e)=>setphoneNo(e.target.value)} value={phoneNo} required />
+                  <label for="NIC">NIC:</label>
+                  <input
+                    type="text"
+                    id="NIC"
+                    name="NIC"
+                    onChange={(e) => setNIC(e.target.value)}
+                    value={NIC}
+                    required
+                  />
 
-    			<label for="date">Date:</label>
-    			<input type="date" id="date" name="date" onChange={(e)=>setdate(e.target.value)} value={date} required />
+                  <label for="phoneNo">Phone No:</label>
+                  <input
+                    type="tel"
+                    id="phoneNo"
+                    name="phoneNo"
+                    onChange={(e) => setphoneNo(e.target.value)}
+                    value={phoneNo}
+                    required
+                  />
 
-    			<label for="status">Status:</label>
-				<select id="status" name="status" onChange={(e)=>setstatus(e.target.value)} value={status} required>
-      				<option value="Pending">Pending</option>
-      				<option value="In Progress">In Progress</option>
-      				<option value="Completed">Completed</option>
-            
-    			</select>
-				
-				<button type="button" onClick={handleSubmit}>Submit</button>
-				{error && <div className="error">error</div>}
-				</form>
-				
-              <Accordion defaultActiveKey="0">
-                
-                <br />
-              </Accordion>
-            </Card.Body>
-          </Card>
-        </div>
-        
-      )}
-		
+                  <label for="date">Date:</label>
+                  <input
+                    type="date"
+                    id="date"
+                    name="date"
+                    onChange={(e) => setdate(e.target.value)}
+                    value={date}
+                    required
+                  />
+
+                  <label for="status">Status:</label>
+                  <select
+                    id="status"
+                    name="status"
+                    onChange={(e) => setstatus(e.target.value)}
+                    value={status}
+                    required
+                  >
+                    <option value="Pending">Pending</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                  </select>
+
+                  <button onClick={handleSubmit}>Submit</button>
+                  {error && <div className="error">error</div>}
+                </form>
+
+                <Accordion defaultActiveKey="0">
+                  <br />
+                </Accordion>
+              </Card.Body>
+            </Card>
+          </div>
+        )}
+
         <table class="Repair">
           <tr>
             <th>Repair ID</th>
@@ -211,7 +231,6 @@ function Repair() {
             <td>Pending</td>
           </tr>
         </table>
-        
       </div>
       <div class="RepairContainer2">
         <h2>Billing</h2>
@@ -238,86 +257,101 @@ function Repair() {
           <tr>
             <td>Date</td>
             <td>09-01-2024</td>
-          </tr> 
+          </tr>
           <tr>
             <td>Parts</td>
             <td>
-              
-            <ThemeProvider theme={theme}>
-        <Stack spacing={2} direction="row">
-    
-      <Button variant="contained" onClick={togglePartsForm} style={{ color: 'white',textTransform:'none' }}>Add new Repair Record  </Button>
+              <Stack spacing={2} direction="row">
+                <Button
+                  variant="contained"
+                  onClick={togglePartsForm}
+                  style={{ color: "white", textTransform: "none" }}
+                >
+                  Add new Repair Record{" "}
+                </Button>
+              </Stack>
 
-    </Stack>
-    </ThemeProvider>
               {isPartsFormOpen && (
-     //Add new parts form
-           
-        <div className="Add-new-parts-Form">
-          <Card className="Add-New-parts-card">
+                //Add new parts form
 
-          <Card.Body>
-            <h2>Add Parts</h2>
-            <form action="/submit" method="post">
-                   <label for="part">Part:</label>
-                   <input type="text" id="part" name="part" required></input>
-            
-                   <label for="quantity">Quantity:</label>
-                   <input type="number" id="quantity" name="quantity" required></input>
+                <div className="Add-new-parts-Form">
+                  <Card className="Add-New-parts-card">
+                    <Card.Body>
+                      <h2>Add Parts</h2>
+                      <form action="/submit" method="post">
+                        <label for="part">Part:</label>
+                        <input
+                          type="text"
+                          id="part"
+                          name="part"
+                          required
+                        ></input>
 
-                   <label for="price">Price per unit:</label>
-                   <input type="number" id="price" name="price" required></input>
+                        <label for="quantity">Quantity:</label>
+                        <input
+                          type="number"
+                          id="quantity"
+                          name="quantity"
+                          required
+                        ></input>
 
-                  <label for="totalPrice">Total Price:</label>
-                  <input type="number" id="totalPrice" name="totalPrice" readonly></input>
+                        <label for="price">Price per unit:</label>
+                        <input
+                          type="number"
+                          id="price"
+                          name="price"
+                          required
+                        ></input>
 
-                  <button type="submit">Submit</button>
-            
-            </form>
-            
-            <Accordion defaultActiveKey="0">
-                
-                <br />
-              </Accordion>
-            </Card.Body>
-            </Card>
-            </div>
+                        <label for="totalPrice">Total Price:</label>
+                        <input
+                          type="number"
+                          id="totalPrice"
+                          name="totalPrice"
+                          readonly
+                        ></input>
 
+                        <button type="submit">Submit</button>
+                      </form>
+
+                      <Accordion defaultActiveKey="0">
+                        <br />
+                      </Accordion>
+                    </Card.Body>
+                  </Card>
+                </div>
               )}
-              
             </td>
           </tr>
-                </table>
-     <br />
-     
+        </table>
+        <br />
 
-          <table class="parts">
-            <tr>
-              <th>part</th>
-              <th>quantity</th>
-              <th>price</th>
-              <th>Total price</th>
-            </tr>
-            <tr>
-              <td>Part 1</td>
-              <td>5</td>
-              <td>$10.00</td>
-              <td>$50.00</td>
-            </tr>
+        <table class="parts">
+          <tr>
+            <th>part</th>
+            <th>quantity</th>
+            <th>price</th>
+            <th>Total price</th>
+          </tr>
+          <tr>
+            <td>Part 1</td>
+            <td>5</td>
+            <td>$10.00</td>
+            <td>$50.00</td>
+          </tr>
 
-            <tr>
-              <td>Part 2</td>
-              <td>2</td>
-              <td>$15.00</td>
-              <td>$30.00</td>
-            </tr>
-          </table>
-        
+          <tr>
+            <td>Part 2</td>
+            <td>2</td>
+            <td>$15.00</td>
+            <td>$30.00</td>
+          </tr>
+        </table>
+
         <hr />
-        
-        
+
         <h2>services</h2>
-        
+
         <table class="services">
           <tr>
             <td>
@@ -347,7 +381,6 @@ function Repair() {
       </div>
     </div>
   );
-		};
-	
+}
 
 export default Repair;
