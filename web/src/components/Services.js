1e3df48405f449f340fcd4b6ab3f0c335f156111
Himantha-alt <jayashanhimantha@gmail.com>
2024-01-19 15:33:18 +0530

    Update Services.js

--- web/src/components/Services.js
+++ web/src/components/Services.js
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/system";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
@@ -15,19 +16,56 @@ const Services = () => {
     <div className="services-container">
       <div className="services-row-1">
         <div className="services-ow-1-column-1">
-          <span className="services-title">Vehicle Categories</span>{" "}
+          <span className="services-title">Services Offered</span>{" "}
           <FormGroup sx={{ display: "flex", flexdirection: "row" }}>
             <FormControlLabel
               control={<Checkbox size="small" />}
-              label={<MediumLabel>Light Weight</MediumLabel>}
+              label={<MediumLabel>Suspension Repairs</MediumLabel>}
             />
+
             <FormControlLabel
               control={<Checkbox size="small" />}
-              label={<MediumLabel>Light Weight</MediumLabel>}
+              label={<MediumLabel>Transmission Issues</MediumLabel>}
             />
+
             <FormControlLabel
               control={<Checkbox size="small" />}
-              label={<MediumLabel>Light Weight</MediumLabel>}
+              label={<MediumLabel>Electrical</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>Electronic</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>Body Repairs & Painting</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>Breakdown Repair and Services</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>Engine</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>Scanning</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>HV System</MediumLabel>}
+            />
+
+            <FormControlLabel
+              control={<Checkbox size="small" />}
+              label={<MediumLabel>Brake Services and Maintenance</MediumLabel>}
             />
           </FormGroup>
           <Box
@@ -38,12 +76,14 @@ const Services = () => {
             noValidate
             autoComplete="off"
           >
-            <TextField
-              label="Size"
-              id="outlined-size-small"
-              defaultValue="Small"
-              size="small"
-            />
+            <TextField label="Add More" id="outlined-size-small" size="small" />
+            <Button
+              variant="contained"
+              style={{ width: "80px", color: "white", top: "16px" }}
+            >
+              {" "}
+              Add
+            </Button>
           </Box>
         </div>
       </div>
