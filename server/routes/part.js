267acf79c6fb75fdf9670462cf0289e4605823b6
Imalshakarunarathna <imalsha.karunarathna07@gmail.com>
2024-01-20 16:28:05 +0530

    parts price calculating

--- server/routes/part.js
+++ server/routes/part.js
@@ -3,14 +3,12 @@ const {
   createPart,
   deletePart,
   getAllParts,
-  getAllPartsByGarageId,
-  insertManyParts,
+ 
 } = require("../controllers/partController");
 const router = express.Router();
 
 router.post("/", createPart);
 router.get("/", getAllParts);
 router.delete("/:id", deletePart);
-router.get("/:garageId", getAllPartsByGarageId);
-router.post("/n", insertManyParts);
+
 module.exports = router;
