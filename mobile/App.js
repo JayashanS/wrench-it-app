9c81c89f752a1fab5d33d8fa50a1098f38d6fc93
Himantha-alt <jayashanhimantha@gmail.com>
2024-01-27 13:38:10 +0530

    Updated

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// App.js
+import React from "react";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import TabNavigator from "./routes/TabNavigator";
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
       <Stack.Navigator>
+<<<<<<< Updated upstream
         <Stack.Screen name="Wrench it" component={TabNavigator} />
+=======
+        <Stack.Screen
+          name="Main"
+          options={{ headerShown: false }}
+          component={TabNavigator}
+        />
+>>>>>>> Stashed changes
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
