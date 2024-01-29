bdb8618264dcc3a5922c918d4755541d6d09e700
Imalshakarunarathna <imalsha.karunarathna07@gmail.com>
2024-01-29 18:19:28 +0530

    Assistance Page completed

---
+++ mobile/screens/Request/RequestSuccess.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RequestSuccess = () => {
+  return (
+    <View>
+      <Text>Community Screen</Text>
+    </View>
+  );
+};
+
+export default RequestSuccess;
