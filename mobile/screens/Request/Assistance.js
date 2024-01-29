08e29b0e647bdbafa808fe51c19cd6b124f27695
Imalshakarunarathna <imalsha.karunarathna07@gmail.com>
2024-01-29 12:16:29 +0530

    profile page completed and navigate to assistance

---
+++ mobile/screens/Request/Assistance.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Assistance = () => {
+  return (
+    <View>
+      <Text>Assistance Screen</Text>
+    </View>
+  );
+};
+
+export default Assistance;
