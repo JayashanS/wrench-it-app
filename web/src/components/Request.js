7867ac04f4a203b117202af0b1d3cc42289d1f82
Pramodmadhushan50 <125783109+Pramodmadhushan50@users.noreply.github.com>
2024-01-31 00:58:32 +0530

    updated Request.js & Request.css

--- web/src/components/Request.js
+++ web/src/components/Request.js
@@ -9,6 +9,7 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import Directions from "./Directions"
 import MessageBox from "./MessageBox";
+import { lightBlue } from "@mui/material/colors";
 
 
 export default function Request() {
@@ -40,7 +41,7 @@ export default function Request() {
               Matara <br />
             </div>
             <div className="button-box">
-              <button className="custom-button">View</button>
+              <button className="custom-button" style={{textAlign:"center"}}>View</button>
               <button
                 className="custom-button"
                 style={{ backgroundColor: "red" }}
@@ -66,7 +67,7 @@ export default function Request() {
               Matara <br />
             </div>
             <div className="button-box">
-              <button className="custom-button">View</button>
+              <button className="custom-button" style={{textAlign:"center"}}>View</button>
               <button className="custom-button"style={{ backgroundColor: "red" }}>
                 Decline
               </button>
@@ -101,6 +102,27 @@ export default function Request() {
             <MessageBox/>
           </div>
           <div className="desicion-box">
+            <div className="profile-circle" style={{marginLeft:150,width:"30%",height:"42%"}}></div>
+            <div className="vehicle-detail">
+            <div className="detail-boxes">
+              <div className="info" style={{marginLeft:"12px"}}><p><strong>Vehicle:</strong><br/> Toyota Camry - Sedan</p></div>  
+            </div>
+            <div className="detail-boxes">
+              <div className="info" style={{marginLeft:"12px"}}><p><strong>Location:</strong><br/>Matara</p></div>  
+            </div>
+            <div className="detail-boxes">
+              <div className="info" style={{marginLeft:"12px"}}><p><strong>Issue:</strong><br/> Won't Start</p></div>  
+            </div>
+            <div className="detail-boxes">
+              <div className="info" style={{marginLeft:"12px",borderRight:"none"}}><p><strong>Contact:</strong><br/> +9471000000</p></div>  
+            </div>
+            </div>
+
+           <div className="buttons">
+              <button className="accept" style={{marginLeft:15}}>Accept</button>
+              <button className="hold" style={{marginLeft:120}}>Hold</button>
+              <button className="decline" style={{marginLeft:120}}>Decline</button>
+            </div>
           </div>
         </div>
 
