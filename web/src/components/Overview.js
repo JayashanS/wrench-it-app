80cca4c3ed8e88f1ddf2216c807f5bd51d3a4e75
Himantha-alt <jayashanhimantha@gmail.com>
2024-02-18 16:20:06 +0530

    Update Overview.js

--- web/src/components/Overview.js
+++ web/src/components/Overview.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
 import "../styles/Overview.css";
 
 export default function Overview() {
@@ -20,12 +21,34 @@ export default function Overview() {
   const currentDate = time.toLocaleDateString("en-US", {
     timeZone: "Asia/Colombo",
   });
+  const uData = [40, 30, 20, 27, 18, 23, 34];
+  const pData = [20, 13, 40, 39, 48, 38, 43];
+  const cDate = new Date();
+
+  const daysOfWeek = Array.from({ length: 7 }, (_, i) => {
+    const pastDate = new Date(cDate);
+    pastDate.setDate(cDate.getDate() - i);
+    const month = pastDate.getMonth() + 1;
+    const dayOfMonth = pastDate.getDate();
+    return `${month}/${dayOfMonth}`;
+  });
+
+  const xLabels = daysOfWeek.reverse();
 
   return (
     <div className="overview-container">
       <h1>Current Time:</h1>
       <p>Time: {currentTime}</p>
       <p>Date: {currentDate}</p>
+      <LineChart
+        width={500}
+        height={250}
+        series={[
+          { data: pData, label: "Resevations" },
+          { data: uData, label: "Requests" },
+        ]}
+        xAxis={[{ scaleType: "point", data: xLabels }]}
+      />
     </div>
   );
 }
