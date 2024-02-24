d930747800ab4c10aabaefc2288fa55b10a06158
Sajazah <153173738+Sajazah@users.noreply.github.com>
2024-02-24 09:06:59 +0530

    Sajadh

--- mobile/routes/ChatNavigator.js
+++ mobile/routes/ChatNavigator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Chat from "../screens/Chat/Call";
+import Chat from "../screens/Chats/Call";
 
 const Stack = createStackNavigator();
 
