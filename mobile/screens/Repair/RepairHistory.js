4e2462a495663f9e65c7103682743541d8eab59f
Pramodmadhushan50 <125783109+Pramodmadhushan50@users.noreply.github.com>
2024-04-23 23:14:07 +0530

    Update RepairHistory.js

--- mobile/screens/Repair/RepairHistory.js
+++ mobile/screens/Repair/RepairHistory.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const RepairHistory = () => {
   const historyData = [
@@ -14,6 +15,47 @@ const RepairHistory = () => {
     },
   ];
 
+  const [email, setEmail] = useState("");
+  const [repairData, setRepairData] = useState([]);
+
+  useEffect(() => {
+    const fetchEmailFromAsyncStorage = async () => {
+      try {
+        const userData = await AsyncStorage.getItem("user");
+        if (userData) {
+          const { email } = JSON.parse(userData);
+          setEmail(email);
+          fetchRepairs(email);
+        }
+      } catch (error) {
+        console.error("Error fetching email from AsyncStorage:", error);
+      }
+    };
+
+    fetchEmailFromAsyncStorage();
+  }, []);
+
+  useEffect(() => {
+    fetchRepairs(email);
+
+  }, [email]);
+
+  const fetchRepairs = async (userEmail) => {
+    try {
+      const response = await fetch(
+        `http://${process.env.EXPO_PUBLIC_IP}:4000/api/request/user/${userEmail}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch repairs");
+      }
+      const data = await response.json();
+      setRepairData(data);
+    } catch (error) {
+      console.error("Error fetching repairs:", error);
+    }
+  };
+
+
   return (
     <ScrollView>  
       <View style={styles.container}>
@@ -22,11 +64,11 @@ const RepairHistory = () => {
           <Text style={styles.text}>Repair History</Text>
         </Text>
 
-        {historyData.map((item) => (
-          <View key={item.id} style={styles.card}>
+        {repairData.map((item) => (
+          <View key={item._id} style={styles.card}>
             <Text style={styles.title}>{item.name}</Text>
             <Text style={styles.address}>{item.address}</Text>
-            <Text style={styles.vehicle}>Vehicle: {item.vehicle}</Text>
+            <Text style={styles.vehicle}>Vehicle: {item.model}</Text>
             <Text style={styles.date}>Started On {item.date}</Text>
             <Text style={styles.fault}>Fault: {item.fault}</Text>
 
